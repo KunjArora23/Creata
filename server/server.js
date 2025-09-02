d3f2ab4ba705c7b0e7741cf263bca4a9443758fa
KunjArora23 <141988347+KunjArora23@users.noreply.github.com>
2025-09-02 20:08:13 +0530

    Set default CORS origin to localhost if undefined

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 console.log(process.env.FRONTEND_URL);
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
 }))
 
